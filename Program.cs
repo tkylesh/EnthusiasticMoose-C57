7661d51c1df61331c51385a71d94054bbb476f24
tkylesh <tks2n@mtmail.mtsu.edu>
2022-08-20 17:22:21 -0500

    Enthusiastic moose complete through phase 9.

--- Program.cs
+++ Program.cs
@@ -2,15 +2,83 @@
 
 
 
-Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
-Console.WriteLine("--------------------------------------------");
-Console.WriteLine();
+Main();
+
+void Main()
+{
+    Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
+    Console.WriteLine("--------------------------------------------");
+    Console.WriteLine();
+
+    // Let the moose speak!
+    MooseSays("H I, I'M  E N T H U S I A S T I C !");
+    MooseSays("I really am enthusiastic");
+
+    // Ask a question
+    CanadaQuestion();
+    EnthusiasticQuestion();
+    LoveCSharpQuestion();
+    SecretQuestion();
+}
+
+void CanadaQuestion()
+{
+    bool isTrue = MooseAsks("Is Canada real?");
+    // Console.WriteLine(isTrue);
+    if (isTrue)
+    {
+        MooseSays($"Really? it seems very unikely.");
+    }
+    else
+    {
+        MooseSays("I Knew IT!!!");
+    }
+}
+
+void EnthusiasticQuestion()
+{
+    bool isTrue = MooseAsks("Are you enthusiastic?");
+    // Console.WriteLine(isTrue);
+    if (isTrue)
+    {
+        MooseSays($"Yay!");
+    }
+    else
+    {
+        MooseSays("You should try it!!!");
+    }
+}
+
+void LoveCSharpQuestion()
+{
+    bool isTrue = MooseAsks("Do you love c# yet?");
+    // Console.WriteLine(isTrue);
+    if (isTrue)
+    {
+        MooseSays($"Good job sucking up to your instructor!");
+    }
+    else
+    {
+        MooseSays("You will...oh, yes, you will...");
+    }
+}
+
+void SecretQuestion()
+{
+    bool isTrue = MooseAsks("Do you want to know a secret?");
+    // Console.WriteLine(isTrue);
+    if (isTrue)
+    {
+        MooseSays($"ME TOO!!!! I love secrets...tell me one!");
+    }
+    else
+    {
+        MooseSays("Oh, no ...secrets are the best, I love to share them!");
+    }
+}
 
 
-
-MooseSays("Hello!");
-
-static void MooseSays(string message)
+void MooseSays(string message)
 {
     Console.WriteLine($@"
                                       _.--^^^--,
@@ -40,4 +108,26 @@ static void MooseSays(string message)
                       \ \'._  ` '_.'
                        `^^` `^^^`
     ");
+}
+
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    //while invalid answer continue to ask
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
